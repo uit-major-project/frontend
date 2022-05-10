92c535e1b7617309f0046c208ff2df85e9c3960d
Prince Agrawal <prince.agrawal@koireader.com>
2022-05-11 03:08:02 +0530

    setup sentry

--- next.config.js
+++ next.config.js
@@ -6,10 +6,27 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
 const withPlugins = require('next-compose-plugins');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withPlugins([withBundleAnalyzer, nextConfig]);
+module.exports = withSentryConfig(
+  withPlugins([withBundleAnalyzer, nextConfig]),
+  sentryWebpackPluginOptions
+);
